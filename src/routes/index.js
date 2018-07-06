580b94e6aa2b63bc8cd5cd4689c759ebebfcd19f
Welington da Silva <thebylito@gmail.com>
2018-07-06 15:19:15 -0400

    nova estrutura

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from 'react-navigation';
+import HomeScreen from '../screens/HomeScreen';
+
+export default createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+});
